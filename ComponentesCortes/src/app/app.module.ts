48294565c6b785d8dbd2a50dc287b7da38b549ed
jeiss <jeissoncortes02@outlook.com>
2022-07-15 10:34:07 -0500

    avance

--- ComponentesCortes/src/app/app.module.ts
+++ ComponentesCortes/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
